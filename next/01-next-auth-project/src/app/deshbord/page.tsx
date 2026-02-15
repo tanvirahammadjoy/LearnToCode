fd2e49c1a9bfb4286c6b20acf036e6b0adf49d86
tanbir ahammad <tavirahammadjoy@gmail.com>
2026-02-16 00:33:34 +0800

    Remove Next.js authentication project files and configurations

--- next/01-next-auth-project/src/app/deshbord/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div className="p-10">
-      <h1 className="text-2xl">Protected Dashboard</h1>
-      <p>Only logged in users can see this</p>
-    </div>
-  );
-}
