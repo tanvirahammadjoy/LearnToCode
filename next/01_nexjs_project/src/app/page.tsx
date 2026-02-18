67482c0bf9c78b6f387f7e1f01c4e0e1102e216f
tanbir ahammad <tavirahammadjoy@gmail.com>
2026-02-19 01:08:29 +0800

    feat: add user authentication and user management features
    - Added new dependencies: axios, bcryptjs, dotenv, jsonwebtoken, nodemailer, and react-hot-toast.
    - Implemented user fetching from the database in the Home component.
    - Created a navigation component (Nab) for easy access to Home, About, and Blog pages.
    - Added About and Blog pages with basic structure.
    - Implemented API routes for user authentication (login, logout, register) and user data retrieval.
    - Established MongoDB connection utility for database operations.
    - Defined Mongoose models for Users and Movies with appropriate schemas.

--- next/01_nexjs_project/src/app/page.tsx
+++ next/01_nexjs_project/src/app/page.tsx
@@ -1,9 +1,32 @@
-import Image from "next/image";
+"use client";
+
+import Nab from "../components/Nab";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Nab />
       <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      </main>
+      <div>
+        <h1>Users</h1>
+
+        {users.map((user: any) => (
+          <div key={user._id}>
+            <h2>name: {user.name}</h2>
+            <p>email: { user.email}</p>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
---
+++ next/01_nexjs_project/src/models/Users.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { collection: "users" },
+);
+
+export const User = mongoose.model("User", UserSchema);
