b1f6e4a93f647c94925a58a8cb9e447ca6e8e588
tanbir ahammad <tavirahammadjoy@gmail.com>
2026-03-01 04:28:07 +0800

    feat: create Post component to fetch and display blog posts

--- nextjsprojects/05_nextjs_with_document/src/app/blog/page.tsx
+++ nextjsprojects/05_nextjs_with_document/src/app/blog/page.tsx
@@ -1,10 +1,10 @@
-export default async function Page() {
+import Post from "@/ui/post";
 
+export default async function Page() {
   return (
-    <ul>
-        <li>Blog Post 1</li>
-        <li>Blog Post 2</li>
-        <li>Blog Post 3</li>
-    </ul>
+    <div>
+      <h1>Blog Page</h1>
+      <Post />
+    </div>
   );
 }
--- nextjsprojects/05_nextjs_with_document/src/app/page.tsx
+++ nextjsprojects/05_nextjs_with_document/src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const filters = (await searchParams).filters;
   return (
     <div>
       <h1>Home Page</h1>
       <Image src="/next.svg" alt="Profile" width={100} height={100} />
+      <p>Filters: {filters}</p>
     </div>
   );
 }
